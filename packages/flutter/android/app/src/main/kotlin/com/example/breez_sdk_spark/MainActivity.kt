77a1c73d4b02a10c46868a66c435bd9779d802e8
Jesse de Wit <witdejesse@hotmail.com>
2025-06-05 14:41:07 +0200

    use a plugin flutter project

--- packages/flutter/android/app/src/main/kotlin/com/example/breez_sdk_spark/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.breez_sdk_spark
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ packages/flutter/example/android/app/src/main/kotlin/breez/breez_sdk_spark_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package breez.breez_sdk_spark_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
